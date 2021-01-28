b45f7607fb0ebac3f8004e748a44ef4e18985617
Courtney Fox <courtney.fox@me.com>
2021-01-28 11:41:09 -0500

    created card body for current weather in html

--- Assets/script.js
+++ Assets/script.js
@@ -1,15 +1,21 @@
-$(document).ready(function() {
+$( document ).ready(function() {
+    var DateTime = luxon.DateTime
+    var currentDate = DateTime.local()
+    var currentDateString = currentDate.toLocaleString(DateTime.DATE_SHORT)
+
     // Create click listener for 'search' button
     $('#search-btn').on("click", function() {
         var city = $('#search-city').val()
-        getForecast(city)
+        saveSearchRecord(city)
+        showSearchHistory()
+        searchWeather(city)
 
         // clear search
-        $('#search-val').val("")
+        $('#search-city').val("")
     })
 
-    // Retrieve the forecast for the city entered in search bar
-    function getForecast(city) {
+    // Search for the weather for the city entered in search bar
+    function searchWeather(city) {
         $.ajax({
             url: "https://api.openweathermap.org/data/2.5/forecast",
             data: {
@@ -17,20 +23,60 @@ $(document).ready(function() {
                 appid: "6617c4c00e3f102ebf0972604493763d",
                 unit: "imperial"
             },
-            success: function(data) {
-                console.log("data", data);
-            }
+            success: parseWeatherData
         })
     }
 
-    // Create list of cities entered in search bar
-        // append new entries to the bottom of the list
+    function parseWeatherData(data) {
+        console.log("weather data", data);
+        // TODO: Data needs to include city, the current date, an icon that represents the current
+        // weather condition, temp, humidity, wind speed, and UV index
+        // UV Index number should have a box around it that changes color, depending on the value
+        console.log("data.city.name", data.city.name)
+        console.log("currentDateString", currentDateString)
+        data.list.forEach(element => {
+            console.log(element.dt_txt)
+        })
+    }
 
+    function saveSearchRecord(city) {
+        // 1. Load search history from localStorage. If nothing is there, default to an empty array.
+        var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryData")) || []
+        // 2. Push the `city` into the search history array
+        searchHistoryArr.push(city)
+        // 3. Store the updated search history in localStorage
+        localStorage.setItem("searchHistoryData", JSON.stringify(searchHistoryArr))
+    }
+
+    function showSearchHistory() {
+        // Load search history from localStorage. If nothing is there, default to an empty array.
+        var searchHistoryArr = JSON.parse(localStorage.getItem("searchHistoryData")) || []
+
+        // Create list of cities entered in search bar
+        var searchHistoryDiv = $('#search-history')
+        searchHistoryDiv.empty()
+        searchHistoryArr.forEach(element => {
+            var row = $('<a>', {class: 'list-group-item list-group-item-action'})
+            row.text(element)
+            // append new entries to the bottom of the list
+            searchHistoryDiv.append(row)
+        });
+
+        // Get all list-group-item elements and add an on click listener
+        $('.list-group-item').on('click', (e) => {
+            console.log('list-group-tem clicked', e.target.textContent);
+            // Call searchWeather function and pass in e.target.textContent
+            searchWeather(e.target.textContent)
+        })
+    }
 
     // Create 'card' class for today's weather data
-        // Data needs to include city, the current date, an icon that represents the current weather condition, temp, humidity, wind speed, and UV index
-            // UV Index number should have a box around it that changes color, depending on the value
 
     // Create a 'card' class for the 5 day forecast
         // Data needs to include the following 5 dates, an icon representing the weather condition on each day, temp, and humidity
+
+
+
+    showSearchHistory()
+
 })
